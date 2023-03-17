9ef45760e61f78922010eaf88eb57d3233a34ec0
Nafas Ebrahimi <ebranafas@gmail.com>
2023-03-17 11:12:19 +0330

    made seperate component for file input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import FileInput from './components/FileInput'
 
 function App() {
-
   return (
     <div className="app_container">
       <h1>
@@ -10,15 +10,7 @@ function App() {
       <p>
         Upload a audio, change name by us 🙌
       </p>
-      <label htmlFor='uploadAudio'>
-        <div className='uploadAudio_button'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-          </svg>
-          <span>Click to upload</span>
-        </div>
-        <input type="file" />
-      </label>
+      <FileInput />
     </div>
   )
 }
