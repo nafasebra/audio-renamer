90582306d51214ec6a05ce4566fd7673252e55bf
Nafas Ebrahimi <ebranafas@gmail.com>
2023-03-19 10:25:29 +0330

    wrote a function for get file type

--- src/libs/ChangeFilename.ts
+++ src/libs/ChangeFilename.ts
@@ -1,15 +1,16 @@
-const options = {
-  include: ['TALB', 'TIT2'],
-  exclude: ['APIC'], 
-  onlyRaw: false,
-  noRaw: false 
-}
 import * as id3 from 'id3js'
 
-export function handleChangeFilename(file: any) {
-  id3.fromFile(file)
-      .then((tags) => {
+function getFileType (path: string) {
+  const arr = path.split('.');
+  return arr[arr.length - 1]
+}
+
+export function handleChangeFilename(filePath: any) {
+  const fileType = getFileType(filePath.name);
+  id3.fromFile(filePath)
+      .then((tags: any) => {
         console.log(tags)
+        // const renamedFile = new File([filePath], `${tags.title} | ${tags.artist}.${fileType}`, {type: fileType})
       })
       .catch((err) => console.error(err))
 }
