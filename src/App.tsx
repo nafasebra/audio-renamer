439a947bf2dccf8a5083b8ab2728c77806590e56
Nafas Ebrahimi <ebranafas@gmail.com>
2023-03-20 18:52:51 +0330

    handle app provider around of the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import FileInput from './components/FileInput'
 import MusicBox from './components/MusicBox'
 
+import { AppProvider } from './context/AppContext'
+
 function App() {
   return (
-    <div className="app_container">
-      <h1>
-        Audio Renamer
-      </h1>
-      <p>
-        Upload a audio, change name by us 🙌
-      </p>
-      <MusicBox />
-      {/* <FileInput /> */}
-    </div>
+    <AppProvider>
+      <div className="app_container">
+        <h1>
+          Audio Renamer
+        </h1>
+        <p>
+          Upload a audio, change name by us 🙌
+        </p>
+        <MusicBox />
+        {/* <FileInput /> */}
+      </div>
+    </AppProvider>
   )
 }
 
