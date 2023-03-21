124c105b84a5dad85442b36d7e6bce53e0c07940
Nafas Ebrahimi <ebranafas@gmail.com>
2023-03-21 18:46:12 +0330

    change lib folder to utils

---
+++ src/utils/index.ts
@@ -0,0 +1,28 @@
+import * as id3 from "id3js";
+
+function getFileType(path: string) {
+  const arr = path.split(".");
+  return arr[arr.length - 1];
+}
+
+export function getTagFromFile(file: any) {
+  let title: string = "",
+    artist: string = "";
+  id3
+    .fromFile(file)
+    .then((tags) => {
+      title = tags?.title || "";
+      artist = tags?.artist || "";
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
+
+  return [title, artist];
+}
+
+export function handleRenameFile(file: any, title: string, artist: string) {
+  const fileType = getFileType(file.name);
+  // const renamedFile = new File([filePath], `${tags.title} | ${tags.artist}.${fileType}`, {type: fileType})
+}
